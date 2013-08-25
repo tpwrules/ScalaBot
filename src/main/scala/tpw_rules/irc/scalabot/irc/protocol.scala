a1e4b89cade0a21f8b96bf01b5537ee0c94c0ea6
Thomas <twatson52@mac.com>
2013-08-25 12:19:01 -0500

    add basic protocol classes

---
+++ src/main/scala/tpw_rules/irc/scalabot/irc/protocol.scala
@@ -0,0 +1,32 @@
+package tpw_rules.irc.scalabot.irc
+
+import akka.util.{ByteStringBuilder, ByteString}
+
+object protocol {
+  val SP = ByteString(" ")
+}
+
+class protocol {
+  sealed trait ProtocolMessage {
+    val byteString: ByteString
+  }
+
+  sealed trait SimpleProtocolMessage {
+    val value: String
+    val byteString = ByteString(value)
+  }
+
+  case class Source(from: String, user: Option[String], server: Option[String]) extends ProtocolMessage {
+    val byteString = ByteString(List(from, user.getOrElse(""), server.getOrElse("")).mkString(" "))
+  }
+
+  case class Command(value: String) extends SimpleProtocolMessage
+
+  case class Message(source: Option[Source], command: Command, params: List[String], data: Option[String]) {
+    val byteString = new ByteStringBuilder ++=
+      source.map { _.byteString }.getOrElse(ByteString("")) ++=
+      command.byteString ++= protocol.SP ++=
+      ByteString(params.mkString(" ")) ++=
+      ByteString(data.map { " :" + _ } getOrElse "")
+  }
+}
