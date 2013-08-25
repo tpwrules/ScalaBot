59ad2b335d21b85660dbbfd5b38e64d8d6ad1362
Thomas <twatson52@mac.com>
2013-08-25 10:02:03 -0500

    move for sbt

--- src/main/scala/tpw_rules/irc/scalabot/ScalaBot.scala
+++ src/main/scala/tpw_rules/irc/scalabot/ScalaBot.scala
@@ -1,7 +1,10 @@
 package tpw_rules.irc.scalabot
 
+import akka.actor.ActorSystem
 
 object ScalaBot extends App {
+  val system = ActorSystem("")
+
   override def main(args: Array[String]) = {
     println("hi")
   }
